d0a8f5632d3ebe4695d6862fd05ffdf363c9f820
lhob1-dotcom <lhob1@discente.ifpe.edu.br>
2026-03-26 10:23:08 -0300

    Add files via upload

---
+++ atividade/Color.kt
@@ -0,0 +1,11 @@
+package com.example.composenavigationapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val cor1A = Color(0xFF00BCD4)
+val cor2A = Color(0xFF03A9F4)
+val cor3A = Color(0xFF2196F3)
+
+val cor4B = Color(0xFF0368A4)
+val cor5B = Color(0xFF004683)
+val cor6B = Color(0xFF00435D)
